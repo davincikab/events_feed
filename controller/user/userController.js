86eb2d97d72c4ae9dddaa67982db64b2b6c30722
davincikab <davidnganganjeri079@gmail.com>
2021-05-13 13:00:33 -0700

    disable tls certicfication

--- controller/user/userController.js
+++ controller/user/userController.js
@@ -17,7 +17,8 @@ let transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
-    }
+    },
+    tls: {rejectUnauthorized: false}
 });
 
 exports.login = function(req, res) {
